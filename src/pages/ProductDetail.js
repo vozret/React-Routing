4543258c3f87723d94ca866d01e1fecf4299a255
Veronika Ozretic <vozret00@fesb.hr>
2021-06-10 16:03:36 +0200

    extracting route params (271)

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  // returns key-values pairs
+  const params = useParams();
+
+  console.log(params.productId);
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };

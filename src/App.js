2e008c59e98bc8abfb1549c9b2d8925da915ca17
Veronika Ozretic <vozret00@fesb.hr>
2021-06-10 15:03:07 +0200

    Defining and using routes (267)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route } from "react-router-dom";
+import Products from "./components/Products";
+import Welcome from "./components/Welcome";
+// Route is a component
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+// we want to handle different paths on our page and
+// load different components for that paths
